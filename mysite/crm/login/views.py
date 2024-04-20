272d3b630482d917b51c498e31875ad2e6d4bb3a
Luka <96606994+Unbit-studios@users.noreply.github.com>
2024-04-20 12:40:07 +0300

    update

--- mysite/crm/login/views.py
+++ mysite/crm/login/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render, redirect
 
 def index(request):
 	return render(request,'login/login.html')
+
+class LoginUser(DataMixin, LoginView):
+	form_class = AuthenticationForm
+	template_name = 'login/login.html'
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		c_def = self.get_user_context(title="Autorithation")
+		return dict(list(context.items()) + list(c_def.items()))
+		x
